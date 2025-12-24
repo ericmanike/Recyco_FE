492d62dc17c9ca3fe3e020e3ac46e8328ddd1824
Ericsson <manikeeric@gmail.com>
2025-12-24 00:03:58 +0000

    update

--- components/buyContent.tsx
+++ components/buyContent.tsx
@@ -11,7 +11,9 @@ export default function BuyPage() {
   const [filteredProducts, setFilteredProducts] = useState({
     search: '',
     category: '',
-    priceRange: ''
+    priceRange: '',
+    location: '',
+    isSorted: false,
   });
    const searchParams = useSearchParams();
   const router = useRouter();
@@ -37,6 +39,22 @@ export default function BuyPage() {
     params.set("priceRange", value);
     router.push(`?${params.toString()}`);
   };
+
+
+    const locationFilter = (value: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("location", value);
+    router.push(`?${params.toString()}`);
+  };
+
+    const  handleConditionChange = (isSorted: boolean) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set("isSorted", isSorted ? "true" : "false");
+      router.push(`?${params.toString()}`);
+    }
+
+
+  
  useEffect(() => {
     console.log("Filters updated:", { searchfilter, category, priceRange });
   }, [searchfilter, category, priceRange]);
@@ -44,7 +62,7 @@ export default function BuyPage() {
 
 
   return (
-    <div className="flex min-h-screen font-sans  w-full mt-30   bg-blue-100 ">
+    <div className="flex min-h-screen font-sans  w-full md:mt-30 mt-15   bg-blue-100 ">
       
     <div className="grid  grid-cols-1 md:grid-cols-[0.4fr_1fr] py-10 relative ">
     <div className=" ">
@@ -52,23 +70,23 @@ export default function BuyPage() {
       
            <button
             onClick={() => setIsFilterOpen(!isFilterOpen)}
-            className="md:hidden bg-green-500 text-white p-3 rounded-lg w-[80%] m-auto flex items-center justify-center gap-2 mb-8 md:mb-0"
+            className="md:hidden bg-green-500 text-white p-3 rounded-lg w-[80%]   m-auto flex items-center justify-center gap-2 mb-8 md:mb-0"
           >
             <Filter size={20} />
             Filters
           </button>
 
-      <div className={`  md:block  ${isFilterOpen ? 'block' : 'hidden'}  bg-white p-5  m-3 rounded-lg shadow-lg z-100 transition-all duration-100`}>
+      <div className={`  md:block  ${isFilterOpen ? 'block' : 'hidden'}  bg-green-500 space-y-10 text-white p-5  m-3 rounded-lg shadow-lg z-100 transition-all duration-100`}>
         <h2 className="text-xl font-bold mb-4 flex justify-between w-full ">
           <span>Filters</span><button className="text-red-600 cursor-pointer" 
           onClick={() => {
-          setFilteredProducts({ search: '', category: '', priceRange: '' });
+          setFilteredProducts({ search: '', category: '', priceRange: '' , location: '', isSorted: false });
          router.push('?');
           }}>
             Clear all
           </button></h2>
-        <div>
-        <div className="mb-4">
+        <div className="">
+        <div className="mb-4 ">
           <input
             name="search"
             type="text"
@@ -87,7 +105,7 @@ export default function BuyPage() {
           <label className="block mb-2 font-medium">Category</label>
           <select 
           value={filteredProducts.category}
-          className="w-full border border-gray-300 rounded-lg p-2"
+          className="w-full border bg-green-500 border-gray-300 rounded-lg p-2"
            onChange={(e)=>{
             setFilteredProducts(prev => ({...prev, category: e.target.value}));
             categoryFilter(e.target.value) }}>
@@ -103,7 +121,7 @@ export default function BuyPage() {
 
           <select 
           value={filteredProducts.priceRange}
-          className="w-full border border-gray-300 rounded-lg p-2"
+          className="transition-all duration-500 w-full border bg-green-500 border-gray-300 rounded-lg p-2"
            onChange={(e)=>{
             setFilteredProducts(prev => ({...prev, priceRange: e.target.value}));
             priceRangeFilter(e.target.value)
@@ -118,9 +136,68 @@ export default function BuyPage() {
             <option value='over-500'>Over 500GHS</option>
           </select>
           </div>
+
+
+
+
+           <div className="mb-4">
+          <label className="block mb-2 font-medium">Location</label>
+
+          <select 
+          value={filteredProducts.location}
+  
+          className="transition-all duration-500 w-full border bg-green-500 border-gray-300 rounded-lg p-2"
+           onChange={(e)=>{
+            setFilteredProducts(prev => ({...prev, location: e.target.value}));
+            locationFilter(e.target.value)
+
+
+           }}
+           >
+            <option value=''>All Location</option>
+            <option value='accra'>Accra</option>
+            <option value='kumasi'>Kumasi</option>
+            <option value='takoradi'>Takoradi</option>
+            <option value='tamale'>Tamale</option>
+          </select>
+          </div>
+
+         <div className="space-y-2">
+          <label className="block mb-2 font-medium">Condition</label>
+          <label id='sorted' className=" flex flex-nowrap items-center  mr-4">
+           <input 
+           checked={filteredProducts.isSorted === true}
+           onChange={() => {
+              handleConditionChange(true);
+            setFilteredProducts(prev => ({...prev, isSorted: true}));
+          
+           }}
+           type="radio" name="condition"  className="form-checkbox h-5 w-5 text-green-600"
+            />
+           <span className="ml-2 cursor-pointer">Sorted</span>
+          </label>
+          <label id='mixed' className="flex flex-nowrap items-center">
+           <input 
+
+           checked={filteredProducts.isSorted === false}
+           onChange={() => {
+              handleConditionChange(false);
+            setFilteredProducts(prev => ({...prev, isSorted: false}));
+          
+           }}
+           type="radio" name="condition"  className="form-checkbox h-5 w-5 text-green-600"
+           />
+           <span className="ml-2 cursor-pointer">Mixed</span>
+          </label>
+         
+         </div>
+
+
         </div>
+        {/*  Slider   */ }
 
 
+      
      
     </div >
     <div className=" flex flex-wrap justify-center gap-4 ">
